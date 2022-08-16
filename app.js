15fa8f24e0cbce1a5df6c79ee60b81911e979e18
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-08-16 17:57:44 +0900

    .

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/auth", require("./routes/auth"));
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
